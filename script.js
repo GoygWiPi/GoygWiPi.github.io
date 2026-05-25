8f96a122876b8d053ff0732b5511b89f4be785cc
GoygWiPi <gwberczely@gmail.com>
2026-05-25 14:57:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 function fillEmojiLine() {
 
     const line = document.getElementById("emoji-line");
-    line.innerHTML = "😸";
-    const emojiWidth = line.scrollWidth;
+    
+    const temp = document.createElement("span");
+    temp.style.visibility = "hidden";
+    temp.style.position = "absolute";
+    temp.textContent = "😸";
+    document.body.appendChild(temp);
+    const emojiWidth = temp.offsetWidth;
+    document.body.removeChild(temp);
+
     const count = Math.ceil(window.innerWidth / emojiWidth);
     line.innerHTML = "😸".repeat(count);
 }
